c54160973acae3bef0d8cfa60a67ae14c80e9b8e
Ângelo Gabriel <108421347+AngeloGabri@users.noreply.github.com>
2022-11-08 11:12:15 -0300

    Delete Praticando-JS directory

--- Praticando-JS/acordion- js/js/script.js
+++
@@ -1,9 +0,0 @@
-const accordion = document.querySelectorAll('.js-accordion');
-
-accordion.forEach( acc => {
-    acc.addEventListener('click', () =>{
-        let daddy = acc.parentElement;
-
-        daddy.classList.toggle('active');
-    })
-})
